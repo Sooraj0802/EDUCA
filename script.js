9845d053deb467843b65a97db6dba7511591fb2e
Suraj Kumarsur <surajgupta08022002@gmail.com>
2023-07-24 18:54:29 +0530

    comment

---
+++ script.js
@@ -0,0 +1,122 @@
+let menubtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menubtn.onclick = () =>{
+    menubtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+  menuBtn.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".course-slider", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    
+    
+
+    pagination: {
+      el: ".swiper-pagination",
+      clikable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  var swiper = new Swiper(".teachers-slider", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    
+
+    pagination: {
+      el: ".swiper-pagination",
+      clikable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    
+
+    pagination: {
+      el: ".swiper-pagination",
+      clikable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  
+ 
+  let username = "";
+  let password = "";
+document.getElementById("#email").addEventListener(onchange, e => {
+  username = e.target.value;
+  console.log(username);
+  
+  e.preventDefault();
+});
+document.getElementById("#password").addEventListener(onchange, e => {
+  password = e.target.value;
+  console.log(password);
+  
+  e.preventDefault();
+});
+
+
+document.getElementById("#btn").addEventListener(onclick, e => {
+  
+  e.preventDefault();
+});
+
+function login() {
+  // Perform login validation
+  // If login is successful, redirect to the landing page
+  window.location.href = "Secondpage.html";
+  console.log(username);
+  console.log(password);
+}
